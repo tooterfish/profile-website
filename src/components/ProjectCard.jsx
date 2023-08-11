dbdd47afa0593542baa6375992caedc69b5e0cfb
Harry Fleming <email@harryfleming.net>
2023-08-11 15:54:05 +0100

    add tech tags

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,7 +3,7 @@ import './ProjectCard.css'
 import { useState, useEffect } from 'react'
 
 export default function ProjectCard({project, i}) {
-  const {title, subtitle, description, link, code, img} = project
+  const {title, subtitle, description, link, code, img, tech} = project
   const [faded, setFaded] = useState('faded')
 
   useEffect(() => {
@@ -12,11 +12,16 @@ export default function ProjectCard({project, i}) {
 
   return (<>
     <div className={`card ${faded}`}>
-      <h2>{title}</h2>
-      <h4>{subtitle}</h4>
+      <div className={'head'}>
+        <h2>{title}</h2>
+        <h4>{subtitle}</h4>
+      </div>
       <div className={'image'}></div>
-      {description}
-      {code ? <div className={'code'}><a href={code}>code</a></div> : ''}
+      <div className={'description'}>{description}</div>
+      <div className={'foot'}>
+        {code ? <div className={'code'}><a href={code}>code</a></div> : ''}
+        <div className={'tech-container'}>{tech.map(tech => <div className={'tech'}>{tech}</div>)}</div>
+      </div>
     </div>
   </>) 
 }
--- src/project-data.js
+++ src/project-data.js
@@ -8,27 +8,30 @@ const projectData = {
     Ut maxime doloremque qui omnis eaque id obcaecati illum.
     Aut voluptate ducimus ut quia quia et ipsa perferendis non sunt iure qui debitis facilis et maxime Quis est numquam similique?
     `,
+    'img': '',
     'link': '',
     'code': 'dsfsf',
-    'img': '',
+    'tech': ['javascript', 'node', 'postgres', 'react', 'firebase', 'C#', '.NET', 'Django']
   },
   'pro-2': {
     'title': 'Another Project',
     'subtitle': 'Another subtitle',
     'description': `Et labore facilis et asperiores beatae est necessitatibus quaerat sit quia dolor ex blanditiis molestias qui repudiandae odit aut galisum sint.
     `,
-    'link': '',
-    'code': '',
     'img': '',
+    'link': '',
+    'code': 'asdasd',
+    'tech': ['javascript', 'react', 'asdsasa', 'asdsadsad', 'asdsadds']
   },
   'pro-3': {
     'title': 'Project X',
     'subtitle': 'X marks the spot',
     'description': `Et optio maxime At doloremque minus est distinctio voluptas cum quasi aspernatur ut commodi quas et consectetur esse. Non perferendis nisi et quia quae ad deserunt ipsa ut voluptate recusandae sed consectetur voluptas eos laborum dicta sed sint nesciunt.
     `,
+    'img': '',
     'link': '',
     'code': '',
-    'img': '',
+    'tech': ['javascript', 'react', 'firebase']
   },
 }
 
